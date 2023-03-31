324729f537c3658a5e24f91618ca87afe7b1bd1c
ZeMangoFur <zemangofur@gmail.com>
2023-03-31 23:58:59 +0200

    1.2

--- bot.py
+++ bot.py
@@ -624,24 +624,14 @@ async def div(ctx, num, num2):
 async def _lover8(ctx, *, question):
     author_name = ctx.message.author.name
     print('lovetest was used')
-    prc1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
-            57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
-            84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
-
-    await ctx.send(f'The love between {author_name} and {question} is {random.choice(prc1)}%')
+    await ctx.send(f'The love between {author_name} and {question} is {random.randint(1,100)}%')
 
 @client.command(aliases=['gayr8', 'gayrate'])
 @commands.cooldown(rate=1, per=5)
 async def _gayr8(ctx):
     author_name = ctx.message.author.name
     print('gayrate was used')
-    prc1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
-            57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
-            84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
-
-    embed = discord.Embed(title=f'{author_name} is {random.choice(prc1)}% gay',
+    embed = discord.Embed(title=f'{author_name} is {random.randint(1,100)}% gay',
                           color=discord.Color.blue())
     await ctx.send(embed=embed)
 
@@ -650,12 +640,7 @@ async def _gayr8(ctx):
 async def _simpr8(ctx):
     author_name = ctx.message.author.name
     print('simprate was used')
-    prc1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-            30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
-            57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
-            84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
-
-    embed = discord.Embed(title=f'{author_name} is {random.choice(prc1)}% a simp',
+    embed = discord.Embed(title=f'{author_name} is {random.randint(1,100)}% a simp',
                           color=discord.Color.blue())
     await ctx.send(embed=embed)
 
